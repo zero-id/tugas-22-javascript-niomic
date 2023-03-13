1b2d35c5466fcdaecc8f6c259a29b54b8755ca3b
zero-id <abdullatiffebriliana15@gmail.com>
2023-03-13 20:11:38 +0700

    tugas 22 js

---
+++ tugas22.js
@@ -0,0 +1,9 @@
+function tugasduadua() {
+    var kata = ['Saya', 'ingin', 'belajar', 'bersama']
+
+    console.log(kata.forEach(function (item, index, array) {
+        console.log('item :', item, 'index ke', index)
+    }))
+}
+
+tugasduadua()
